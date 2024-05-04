c709032e2ac7044258208042c61689182d77c9ba
dkel3slek <dkel3slek@naver.com>
2024-05-04 21:47:19 +0900

    developSetting

---
+++ src/screens/massageSet.tsx
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  ScrollView,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  Linking,
+  Alert,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+  ImageBackground,
+} from 'react-native';
+import styled from 'styled-components/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useIsFocused} from '@react-navigation/native';
+
+export const MassageSet = ({navigation}: any) => {
+  return (
+    <Container>
+      <Text>마사지 세팅</Text>
+    </Container>
+  );
+};
+
+const Container = styled(SafeAreaView)`
+  flex: 1;
+`;
